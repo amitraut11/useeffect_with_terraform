8ac37110cd279c7b34ede40a629ee5c53efdb704
amit.raut <amitraut11@gmail.com>
2024-03-09 10:37:38 -0600

    added new files

---
+++ src/PersonScore.tsx
@@ -0,0 +1,38 @@
+import React,{ useEffect , useState} from "react";
+import { getPerson } from "./getPerson";
+
+export function PersonScore() {
+    const [name,setName] = useState <String|undefined>();
+    const [score,setScore] = useState (0);
+    const [loading,setLoading] = useState(true);
+
+
+
+  useEffect(() => {
+    getPerson().then((person) => {
+    setLoading(false);
+    setName(person.name); 
+   });
+
+  }, []);
+
+
+  if (loading)
+  {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+    <h1>Hello, {name}!</h1>
+    <p>Your score is {score}</p>
+  
+
+
+    <button onClick = {()=>setScore(score+1)}>Add</button>
+    <button onClick ={()=>setScore(score-1)}>Subtract</button>
+    <button onClick ={()=>setScore(0)}>Reset</button>
+    </div>
+  )
+
+  
+};
---
+++ src/getPerson.ts
@@ -0,0 +1,21 @@
+type Person = {
+
+name : string;
+}
+
+export function getPerson(): Promise<Person> {
+    
+    return new Promise((resolve) => {
+
+setTimeout(() => {
+
+resolve({name: "John"});
+
+}, 1000);
+
+});
+
+}
+
+
+
